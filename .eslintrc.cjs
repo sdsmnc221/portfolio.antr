04a3d8d9d6a88277ab36a9109f9381e76e5001f9
AnTR <vanan.sdmsnc.221@gmail.com>
2023-02-19 21:01:31 +0100

    feat: init

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+/* eslint-env node */
+require("@rushstack/eslint-patch/modern-module-resolution");
+
+module.exports = {
+  root: true,
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/eslint-config-prettier",
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {
+    'no-console':
+      process.env.NODE_ENV === 'production'
+        ? [
+          'warn',
+          {
+            allow: ['warn', 'error'],
+          },
+        ]
+        : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? ['warn'] : 'off',
+    'vue/multi-word-component-names': 'off',
+  },
+};
