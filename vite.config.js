4d52b84fb48e315eb93d408e4aa0783637dac079
sdsmnc221 <vanan.sdsmnc.221@gmail.com>
2023-02-25 10:25:33 +0100

    feat: import .env into vite config and import analytics id

--- vite.config.js
+++ vite.config.js
@@ -1,42 +1,48 @@
 import { fileURLToPath, URL } from 'node:url';
 import { VitePluginFonts } from 'vite-plugin-fonts';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { VitePluginRadar } from 'vite-plugin-radar';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    host: true,
-  },
-  plugins: [
-    vue(),
-    VitePluginFonts({
-      google: {
-        families: ['Montserrat', 'Major Mono Display', 'Yeseva One'],
-      },
-    }),
-    VitePluginRadar({
-      // Google Analytics tag injection
-      analytics: {
-        id: process.env.VITE_ANALYTICS,
-        disable: false,
-      },
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '@modules': fileURLToPath(new URL('./src/components/modules', import.meta.url)),
-      '@elements': fileURLToPath(new URL('./src/components/elements', import.meta.url)),
-      '@utils': fileURLToPath(new URL('./src/utils', import.meta.url)),
+export default defineConfig(({ command, mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    server: {
+      host: true,
     },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "./src/assets/scss/vars/index.scss";`,
+    plugins: [
+      vue(),
+      VitePluginFonts({
+        google: {
+          families: ['Montserrat', 'Major Mono Display', 'Yeseva One'],
+        },
+      }),
+      VitePluginRadar({
+        // Google Analytics tag injection
+        analytics: {
+          id: env.ANALYTICS,
+          disable: false,
+        },
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+        '@modules': fileURLToPath(new URL('./src/components/modules', import.meta.url)),
+        '@elements': fileURLToPath(new URL('./src/components/elements', import.meta.url)),
+        '@utils': fileURLToPath(new URL('./src/utils', import.meta.url)),
       },
     },
-  },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "./src/assets/scss/vars/index.scss";`,
+        },
+      },
+    },
+  };
 });
