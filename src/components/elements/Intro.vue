6230f20a99f38e23edf6c589ee5513a6b2d124dc
sdsmnc221 <vanan.sdsmnc.221@gmail.com>
2023-02-26 03:01:27 +0100

    feat: anim quality terms

--- src/components/elements/Intro.vue
+++ src/components/elements/Intro.vue
@@ -1,4 +1,7 @@
 <script setup>
+import { onMounted, ref } from 'vue';
+import { sample, shuffle } from '@utils';
+
 const props = defineProps({
   head: {
     type: String,
@@ -8,19 +11,87 @@ const props = defineProps({
     type: String,
     default: '',
   },
+  qualityTerms: {
+    type: Array,
+    default: () => [],
+  },
+});
+
+const introText = ref(null);
+const ch = ref(5);
+
+const animTerms = (textNode, shuffleNode, oldTerm = '') => {
+  let inc = 0;
+  let out = 0;
+  let str = sample(oldTerm ? props.qualityTerms.filter((term) => term !== oldTerm) : props.qualityTerms).trim();
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789@$%&,.';
+  let t;
+
+  switch (str.length) {
+    case 4:
+      ch.value = 4;
+      break;
+    case 8:
+      ch.value = str.length - 1;
+      break;
+    default:
+      ch.value = str.length - 0.5;
+      break;
+  }
+
+  const anim = () => {
+    inc++;
+    if (inc % 2 === 0 && out < str.length) {
+      textNode.appendChild(document.createTextNode(str[out]));
+      shuffleNode.innerHTML = '';
+      out++;
+    } else if (out >= str.length) {
+      shuffleNode.innerHTML = '';
+      clearInterval(t);
+      setTimeout(() => animTerms(textNode, shuffleNode, str), 600);
+    } else {
+      shuffleNode.innerHTML = sample(shuffle(chars));
+    }
+  };
+
+  t = setInterval(anim, 50);
+
+  textNode.innerHTML = '';
+};
+
+onMounted(() => {
+  if (introText.value) {
+    const mainNode = introText.value.querySelector('strong em');
+
+    if (mainNode) {
+      mainNode.innerHTML = '';
+
+      const textNode = document.createElement('span');
+      textNode.classList.add('.text');
+      mainNode.appendChild(textNode);
+
+      const shuffleNode = document.createElement('span');
+      shuffleNode.classList.add('.shuffle');
+      mainNode.appendChild(shuffleNode);
+
+      animTerms(textNode, shuffleNode);
+    }
+  }
 });
 </script>
 
 <template>
-  <header class="intro">
+  <header class="intro" :style="`--ch: ${ch}ch;}`">
     <div class="intro__head" v-html="head"></div>
-    <div class="intro__text" v-html="shortText"></div>
+    <div class="intro__text" v-html="shortText" ref="introText"></div>
     <img class="intro__decor" src="/img/grid.svg" alt="" />
   </header>
 </template>
 
 <style lang="scss">
 .intro {
+  --ch: 5ch;
+
   padding: 2rem;
 
   &__head {
@@ -70,9 +141,18 @@ const props = defineProps({
       line-height: $ft-s-medium;
       margin-top: 0.48rem;
 
-      strong {
-        color: $color-link-blue;
+      strong,
+      strong em {
+        font-family: $montserrat;
+        font-size: $ft-s-small;
         font-weight: 700;
+        color: $color-link-blue;
+      }
+
+      strong em {
+        display: inline-block;
+        width: var(--ch);
+        transition: all ease 0.4s;
       }
 
       em {
@@ -108,7 +188,9 @@ const props = defineProps({
 
     &__text {
       max-width: 84vw;
-      p {
+      p,
+      p strong,
+      p strong em {
         font-size: $ft-s-xsmall;
       }
     }
