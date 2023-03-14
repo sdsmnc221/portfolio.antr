85c39eec04f49090c11376f2f4ee87dc4632197e
sdsmnc221 <vanan.sdsmnc.221@gmail.com>
2023-03-14 23:27:48 +0100

    feat: rm wave to migrate wave into wave.antr.tech

--- public/wave/scripts/Helper.php
+++
@@ -1,9 +0,0 @@
-<?php 
-class Helper {
-    public function __consctruct() {}
-    
-    public function random_float ($min=0, $max=100) {
-        return ($min+lcg_value()*(abs($max-$min)));
-     }
-}
-?>
