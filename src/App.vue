f8c53e6820460f22fe65627a71d4ff7eff14c35c
sdsmnc221 <antr.2201@gmail.com>
2025-05-20 13:48:04 +0200

    feat(app): implement router v4

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,23 @@
 <script setup>
 import Loader from '@elements/Loader.vue';
-import Intro from '@elements/Intro.vue';
-import RowGrid from '@modules/RowGrid/RowGrid.vue';
-import Outro from '@elements/Outro.vue';
 
-import projectsAdapter from '@utils/prismic/projectsAdapter';
-import introAdapter from '@utils/prismic/introAdapter';
-import outroAdapter from '@utils/prismic/outroAdapter';
-
-import { ref, watch } from 'vue';
-import { useSinglePrismicDocument, usePrismic } from '@prismicio/vue';
+import { ref } from 'vue';
 
 import '@/assets/scss/global/index.scss';
+import { RouterView } from 'vue-router';
 
-const doc = ref(null);
 const projects = ref([]);
-const intro = ref(null);
-const outro = ref(null);
 
-const { data } = useSinglePrismicDocument('homepage', {
-  fetchLinks: [
-    'project.title',
-    'project.row_images',
-    'project.preview_images',
-    'project.description',
-    'project.video',
-    'project.link',
-    'project.year',
-    'project.display_images',
-  ],
-});
-
-setTimeout(() => {
-  doc.value = data;
-}, 1000);
-
-const prismic = usePrismic();
-
-watch(doc, (newVal) => {
-  if (newVal.value && newVal.value.data) {
-    setTimeout(async () => {
-      projects.value = await projectsAdapter(newVal.value.data.projects, prismic);
-      intro.value = introAdapter(
-        newVal.value.data.body.find((slice) => slice.slice_type === 'intro'),
-        prismic
-      );
-      outro.value = outroAdapter(
-        newVal.value.data.body.find((slice) => slice.slice_type === 'outro'),
-        newVal.value.tags
-      );
-    }, 2400);
-  }
-});
+const updateProjects = ({ projects: projectsFromComponents }) => {
+  projects.value = projectsFromComponents;
+};
 </script>
 
 <template>
   <main>
-    <Intro v-if="intro" v-bind="intro" />
-    <RowGrid :data="projects" />
-    <Outro v-if="outro" v-bind="outro" />
+    <RouterView v-slot="{ Component }">
+      <component :is="Component" @update:projects="updateProjects"> </component>
+    </RouterView>
   </main>
 
   <Transition name="fade">
--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
+import { createMemoryHistory, createRouter } from 'vue-router';
 import { createApp } from 'vue';
+
 import App from './App.vue';
-// import App from './App.static.vue';
+import Home from './Home.vue';
+
 import prismic from '@utils/prismic';
 
-createApp(App).use(prismic).mount('#app');
+const routes = [{ path: '/', component: Home }];
+
+const router = createRouter({
+  history: createMemoryHistory(),
+  routes,
+});
+
+createApp(App).use(router).use(prismic).mount('#app');
